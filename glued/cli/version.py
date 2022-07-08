f68cae981700c2599c266d60488393909d3b051d
semantic-release <semantic-release>
2022-07-08 15:15:23 +0200

    0.2.0
    Automatically generated by python-semantic-release

--- glued/cli/version.py
+++ glued/cli/version.py
@@ -1,7 +1,7 @@
 from argparse import Namespace
 
 # set the version number, which is manged by symantic release process
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 def print_version(cmd: Namespace) -> None:
