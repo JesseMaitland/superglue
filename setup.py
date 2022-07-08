241fe10644b0ed6a8c0dc683d494d4d2d5c1c267
jessemaitland <jesse.maitland@heyjobs.de>
2022-07-08 15:04:11 +0200

    feat: first_release

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
-from setuptools import setup, find_packages
-from pathlib import Path
-
-__version__ = '0.0.0'
-
-
-def get_requirements() -> str:
-    req_path = Path.cwd() / 'requirements' / "distribution.txt"
-    return req_path.read_text()
-
-
-setup(
-    name='glued',
-    version=__version__,
-    author='Jesse Maitland',
-    discription='A cli tool to do some pretty rad stuff!',
-    include_package_data=True,
-    install_requires=get_requirements(),
-    packages=find_packages(exclude=('tests*', 'venv')),
-    entry_points={'console_scripts': ['glued = glued.__main__:main']},
-    python_requires='>=3.8'
-)
+"""
+Needed to allow setuptools to build the project form pyproject.toml
+For more info https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+"""
+from setuptools import setup
+setup()
