df9f5d9ce2ed59551ffe194edcfe2df362af3ce6
Jesse Maitland <jesse.maitland@heyjobs.de>
2022-09-07 21:49:15 +0200

    more testing

--- tests/core/test_project.py
+++ tests/core/test_project.py
@@ -17,11 +17,22 @@ def test_shared_root_directory(project: SuperGlueProject) -> None:
     assert project.shared_root == Path.cwd() / "shared"
 
 
+def test_notebooks_directory(project: SuperGlueProject) -> None:
+    assert project.notebooks == Path.cwd() / "notebooks"
+
+
 def test_create_method(project: SuperGlueProject) -> None:
     project.jobs_root = MagicMock()
     project.shared_root = MagicMock()
+    project.notebooks = MagicMock()
+    project.makefile_template = MagicMock()
 
     project.create()
 
     project.jobs_root.mkdir.assert_called_once_with(exist_ok=True)
     project.shared_root.mkdir.assert_called_once_with(exist_ok=True)
+    project.notebooks.mkdir.assert_called_once_with(exist_ok=True)
+
+
+def test_script_path(project: SuperGlueProject) -> None:
+    pass
