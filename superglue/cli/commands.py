776ac5f2283d8053652fab400d0549027c5d9c06
semantic-release <semantic-release>
2023-01-16 11:16:58 +0000

    0.12.0
    Automatically generated by python-semantic-release

--- superglue/cli/commands.py
+++ superglue/cli/commands.py
@@ -2,7 +2,7 @@ from superglue.cli.utils import validate_account
 from superglue.cli.base import BaseSuperglueCommand
 from superglue.cli.messages import Messages
 
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 
 
 class Version(BaseSuperglueCommand):
