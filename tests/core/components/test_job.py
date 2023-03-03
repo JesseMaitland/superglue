1f7fadf9210e5df28c44ccd9f3a00eca98d537a8
Jesse Maitland <jesse.maitland@heyjobs.de>
2023-03-03 15:01:58 +0100

    add tests for job

---
+++ tests/core/components/test_job.py
@@ -0,0 +1,47 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from superglue.core.components.job import SuperglueJob
+from superglue.core.components.tests import SuperglueTests
+from superglue.environment.config import MODULES_PATH, TESTS_PATH
+
+TEST_JOB_NAME = "a_link_to_the_past"
+
+
+@pytest.fixture(scope="module")
+def job() -> SuperglueJob:
+    return SuperglueJob(job_name=TEST_JOB_NAME)
+
+
+def test_job_job_path_property(job: SuperglueJob) -> None:
+    assert job.job_path == job.root_dir / job.name
+
+def test_job_pys_path_property(job: SuperglueJob) -> None:
+    assert job.pys_path == job.job_path / "py"
+
+def test_job_jars_path_property(job: SuperglueJob) -> None:
+    assert job.jars_path == job.job_path / "jars"
+
+def test_job_job_test_path_property(job: SuperglueJob) -> None:
+    assert job.job_test_path == job.tests.jobs_test_dir / job.name
+
+def test_job_main_script_file_property(job: SuperglueJob) -> None:
+    assert job.main_script_file == job.job_path / "main.py"
+
+def test_job_config_file_property(job: SuperglueJob) -> None:
+    assert job.config_file == job.job_path / "config_base.yml"
+
+def test_job_overrides_file_property(job: SuperglueJob) -> None:
+    assert job.overrides_file == job.job_path / "config_overrides.yml"
+
+def test_job_deployment_config_file(job: SuperglueJob) -> None:
+    assert job.deployment_config_file == job.job_path / "config_merged.yml"
+
+def test_job_superglue_modules_property() -> None:
+    mock = MagicMock()
+
+    job = SuperglueJob(job_name="foo")
+    job.config = mock
+    _ = job.superglue_modules
+
+    mock.get.assert_called_once_with("superglue_modules", {})
+
